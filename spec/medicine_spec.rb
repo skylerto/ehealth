9780db57279455ab4ae259e47c6cb1926467460b
Skyler Layne <skylerclayne@gmail.com>
2016-05-20 16:20:30 -0400

    Spec out interacts_with?

--- spec/medicine_spec.rb
+++ spec/medicine_spec.rb
@@ -18,4 +18,38 @@ describe Ehealth::Medicine do
       expect(med.to_s).to eq(out)
     end
   end
+
+  describe '#interactions' do
+    it 'should start with no interactions' do
+      med = Ehealth::Medicine.new(0, 'advil')
+      expect(med.interactions).to eq([])
+    end
+
+    it 'should add an interaction' do
+      m1 = Ehealth::Medicine.new(0, 'advil')
+
+      m2 = Ehealth::Medicine.new(1, 'alcohol')
+
+      m1.interacts_with(m2)
+      m2.interacts_with(m1)
+
+      expect(m1.interactions).to eq([m2])
+      expect(m2.interactions).to eq([m1])
+
+    end
+
+    it 'should should raise error if interaction exists' do
+      m1 = Ehealth::Medicine.new(0, 'advil')
+
+      m2 = Ehealth::Medicine.new(1, 'alcohol')
+
+      m1.interacts_with(m2)
+      m2.interacts_with(m1)
+
+      expect {
+        m1.interacts_with(m2)
+      }.to raise_error(InteractionExistsError)
+    end
+
+  end
 end
