809a6f77e9c126f61cd373a84ccbb85a751eef22
Skyler Layne <skylerclayne@gmail.com>
2016-05-20 17:01:29 -0400

    add #to_s in interactions

--- lib/ehealth/interactions.rb
+++ lib/ehealth/interactions.rb
@@ -12,5 +12,18 @@ module Ehealth
       medicine2.add_interaction(medicine1)
       @interactions << [medicine1, medicine2]
     end
+  
+    def to_s
+      out = "[ "
+      @interactions.each_with_index { |interaction, i|
+        meds = Ehealth::Medications.new
+        interaction.each { |med|
+          meds.add(med)
+        }
+        out << meds.to_s
+        out << ", " unless i == @interactions.size - 1
+      }
+      out << " ]"
+    end
   end
 end
