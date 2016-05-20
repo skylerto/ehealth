96459f59c99b4043bdf1d75acf6e7452375ea5e7
Skyler Layne <skylerclayne@gmail.com>
2016-05-20 12:20:53 -0400

    Bare minimum Patient impl

---
+++ lib/ehealth/patient.rb
@@ -0,0 +1,23 @@
+module Ehealth
+
+  ##
+  # Represents a patient within the system.
+  class Patient
+    attr_accessor :id, :name
+
+    def initialize(id, name)
+      if id < 0
+        raise InvalidPatientIdError.new 'Invalid id'
+      end
+      @id = id
+      @name = name
+    end
+
+  end
+end
+
+##
+# Raised when a patient is created with an invalid id
+class InvalidPatientIdError < StandardError
+end
+
