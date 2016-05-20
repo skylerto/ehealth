aa6715f07d9342eb0d18d8d0ef94b870927d3c42
Skyler Layne <skylerclayne@gmail.com>
2016-05-20 17:20:05 -0400

    Spec prescription

---
+++ spec/prescriptions_spec.rb
@@ -0,0 +1,12 @@
+describe Ehealth::Prescription do
+  it 'should create a new prescription' do
+    pat = Ehealth::Patient.new(0, 'Meri')
+    med = Ehealth::Medicine.new(0, 'Advil')
+    doc = Ehealth::Physician.new(0, 'house', 'generalist')
+    pres = Ehealth::Prescription.new(pat, med, doc)
+
+    expect(pres.patient).to eq(pat)
+    expect(pres.medicine).to eq(med)
+    expect(pres.doc).to eq(doc)
+  end
+end
