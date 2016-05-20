1771bb55348ae729241930f51862d8154cde6d84
Skyler Layne <skylerclayne@gmail.com>
2016-05-20 12:07:18 -0400

    cli grammar and cli rake task

--- Rakefile
+++ Rakefile
@@ -1,4 +1,6 @@
 require "bundler/gem_tasks"
+require 'irb'
+
 task :default => :spec
 
 task :spec do
@@ -7,4 +9,9 @@ task :spec do
         RSpec::Core::RakeTask.new(:spec)
   rescue LoadError
   end
+end
+
+task :cli do
+  ARGV.clear
+  IRB.start
 end
--- lib/ehealth/cli/cli.rb
+++ lib/ehealth/cli/cli.rb
@@ -1,6 +1,66 @@
 module Ehealth
   module Cli
     class << self
+      attr_accessor :physicians, :patients, :medications, :interations
+
+      ##
+      # Add a new physician with the given +id+, +name+, and +type+.
+      def add_physician(id, name, type)
+        "Adding new #{type}: #{name}"
+      end
+
+      ##
+      # Add a new patient with the given +id+, and +name+.
+      def add_patient(id, name)
+        "Adding new patient #{name}"
+      end
+
+      ##
+      # Add a new +medicine+
+      def add_medication(id, medicine)
+        "Adding #{medicine}"
+      end
+
+      ##
+      # Add a new interaction between +m1+ and +m2+
+      def add_interaction(m1, m2)
+        "New Interaction between #{m1} and #{m2}"
+      end
+
+      ##
+      # Add a new prescription for +patient+, from +doctor+
+      def new_prescription(id, doctor, patient)
+        "Add new prescription for #{patient} from #{doctor}"
+      end
+
+      ##
+      # Remove a medication from the system
+      def remove_medicine(id, medicine)
+        "removing #{medicine}"
+      end
+
+      ##
+      # Display all the prescriptions of a given +medicine+
+      def prescriptions(medicine)
+        "all prescriptions involving #{medicine}"
+      end
+
+      ##
+      # Dangerous prescription report: show all dangerous prescriptions
+      def dpr
+        "Oh no!"
+      end
+
+      ##
+      # Start the REPL
+      def start
+        loop do
+            repl(">> ")
+        end
+      end
+
+      private
+
       def handle_input(input)
         result = eval(input)
         puts(" => #{result}")
@@ -11,11 +71,8 @@ module Ehealth
           handle_input(gets.chomp!)
       end
 
-      def start
-        loop do
-            repl(">> ")
-        end
-      end
     end
   end
 end
+
+Ehealth::Cli.start
