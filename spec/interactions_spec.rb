1dc01003f6e7c69d2d56afbd72c6469e6d7d6948
Skyler Layne <skylerclayne@gmail.com>
2016-05-20 17:01:01 -0400

    fix typo in interactions spec

--- spec/interactions_spec.rb
+++ spec/interactions_spec.rb
@@ -9,4 +9,16 @@ describe Ehealth::Interactions do
     expect(interaction.interactions).to eq([[m1,m2]])
 
   end
+
+  it 'should format properly' do
+    m1 = Ehealth::Medicine.new(0, 'advil')
+    m2 = Ehealth::Medicine.new(1, 'alcohol')
+
+    interaction = Ehealth::Interactions.new
+    interaction.add_interaction(m1, m2)
+
+    out = "[ [ 0: advil, 1: alcohol ] ]"
+    
+    expect(interaction.to_s).to eq(out)
+  end
 end
